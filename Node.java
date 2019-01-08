14ecaa11f11be5c699e0e1b1c1facb027c4b8457
Abubakar Salim <salimabubakar@Abubakars-MacBook-Pro.local>
2019-01-08 01:38:25 +0100

    updated readme

--- Node.java
+++ Node.java
@@ -1,9 +1,53 @@
 import java.util.ArrayList;
 
+//Represents a single server
 public class Node {
     protected int latency, distance, workload;
+    protected String name;
     static ArrayList<Node> servers = new ArrayList<Node>();
-    public Node(int latency, int distance, int workload){
+    static ArrayList<Client> clientList = new ArrayList<Client>();
+    public Node(String name,int latency, int distance, int workload){
+        this.name = name;
+        this.latency = latency;
+        this.distance = distance;
+        this.workload = workload;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getLatency() {
+        return this.latency;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+
+    public int getWorkload() {
+        return this.workload;
+    }
+
+    public static void connectServer(ArrayList<Node> s){
+        for(Node a: s){
+            System.out.println(a.getName()  );
+        }
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node("1", 50, 50, 50);
+        System.out.println("hey");
+        servers.add(a);
+        connectServer(servers);
+    }
+  }
+
+  //Represents a single client
+class Client {
+    protected int latency, distance, workload;
+
+    public Client(int latency, int distance, int workload){
         this.latency = latency;
         this.distance = distance;
         this.workload = workload;
@@ -20,31 +64,6 @@ public class Node {
     public int getWorkload() {
         return this.workload;
     }
-
-    public Node check(ArrayList<Node> servers) {
-        for (Node node : servers) {
-            if(node.getLatency() < 100){
-                System.out.println(node);
-                return node;
-            }
-         }
-        return node;
-    }
-    public static void main(String[] args) {
-        Node n1 = new Node(50, 50, 50);
-        Node n2 = new Node(100, 50, 50);
-        Node n3 = new Node(150, 50, 50);
-
-        servers.add(n1);
-        servers.add(n2);
-        servers.add(n3);
-
-        for (Node node : servers) {
-            if(node.getLatency() < 100){
-                System.out.println(node);
-            }
-         }
-        System.out.println(servers.size());
-    }
-
   }
+
+  
